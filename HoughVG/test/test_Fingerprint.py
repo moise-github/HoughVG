3593ab4cb645c2ddf15721c1b40e2f46077652c3
OUEDRAOGO Moïse <96614239+moise-github@users.noreply.github.com>
2025-01-02 19:18:42 +0100

    Update test_Fingerprint.py

--- HoughVG/test/test_Fingerprint.py
+++ HoughVG/test/test_Fingerprint.py
@@ -2,54 +2,94 @@ import unittest
 from HoughVG import Fingerprint
 import time
 Template_Path_Big='./images/Big_DB'
-DataBase_path_Big='./images/Big_data_minuties'#Database_test
+DataBase_path_Big='./images/fingerprint_2'#Database_test
 
 Template_Path_Little='./images/little_DB'
-DataBase_path_Little='./images/little_data_minuties'#Database_test
+DataBase_path_Little='./images/fingerprint_1'#Database_test
 
 class TestFingerprint_Big_DB(unittest.TestCase):
     def test_fingerprint(self):
         seuil_long=10
         seuil_larg=10
         seuil_rot=5
+        Sc=3
+        precision=0
         start = time.time()
-        Fingerprint.fingerprint(Template_Path_Big,DataBase_path_Big,seuil_long,seuil_larg,seuil_rot)
+        Fingerprint.fingerprint(Template_Path_Big,DataBase_path_Big,seuil_long,seuil_larg,seuil_rot,Sc,precision)
         #print('resultats=', results)
         end = time.time()
-        print("Sequentiel(Big DB):exécution time(en secondes): %f" %float( end - start)) 
+        with open('../results/FingerprintSequentialBigDB.txt','w') as f:
+            print("Sequential(Big DB):exécution time(s): %f" %float( end - start), file=f) 
 
-    def test_fingerprintP(self):
+    def test_fingerprintVG(self):
+        seuil_long=10
+        seuil_larg=10
+        seuil_rot=5
+        Sc=3
+        precision=0
+        start = time.time()
+        Fingerprint.fingerprint_VG(Template_Path_Big,DataBase_path_Big,seuil_long,seuil_larg,seuil_rot,Sc,precision)
+        #print('resultats=', results)
+        end = time.time()
+        with open('../results/FingerprintVGBigDB.txt','w') as f:
+            print("Virtual Grid(Big DB):exécution time(s): %f" %float( end - start),file=f) 
+
+
+    def test_fingerprintVGP(self):
         n_cpu=2
         seuil_long=10
         seuil_larg=10
         seuil_rot=5
+        Sc=3
+        precision=0
         start = time.time()
-        Fingerprint.fingerprintP(Template_Path_Big,DataBase_path_Big,seuil_long,seuil_larg,seuil_rot,n_cpu)
+        Fingerprint.fingerprint_VGP(Template_Path_Big,DataBase_path_Big,seuil_long,seuil_larg,seuil_rot,Sc,n_cpu, precision)
         #print('resultats=', results)
         end = time.time()
-        print("Parallel(Big DB):exécution time(en secondes): %f" %float( end - start)) 
+        with open('../results/FingerprintHybridBigDB.txt','w') as f:
+            print("Hybrid(Big DB):exécution time(s): %f" %float( end - start),file=f) 
 
 class TestFingerprint_little_DB(unittest.TestCase):
     def test_fingerprint(self):
         seuil_long=10
         seuil_larg=10
         seuil_rot=5
+        precision=0
+        Sc=3
         start = time.time()
-        Fingerprint.fingerprint(Template_Path_Little,DataBase_path_Little,seuil_long,seuil_larg,seuil_rot)
+        Fingerprint.fingerprint(Template_Path_Little,DataBase_path_Little,seuil_long,seuil_larg,seuil_rot,Sc,precision)
         #print('resultats=', results)
         end = time.time()
-        print("Sequentiel (little DB):exécution time(en secondes): %f" %float( end - start)) 
+        with open('../results/FingerprintSequentialLittleDB.txt','w') as f:
+            print("Sequentiel (little DB):exécution time(s): %f" %float( end - start),file=f) 
 
-    def test_fingerprintP(self):
+    def test_fingerprintVG(self):
+        seuil_long=10
+        seuil_larg=10
+        seuil_rot=5
+        precision=0
+        Sc=3
+        start = time.time()
+        Fingerprint.fingerprint_VG(Template_Path_Little,DataBase_path_Little,seuil_long,seuil_larg,seuil_rot,Sc,precision)
+        #print('resultats=', results)
+        end = time.time()
+        with open('../results/FingerprintVGLittleDB.txt','w') as f:
+            print("Virtual Grid (little DB):exécution time(s): %f" %float( end - start),file=f) 
+
+
+    def test_fingerprintVGP(self):
         n_cpu=2
         seuil_long=10
         seuil_larg=10
         seuil_rot=5
+        precision=0
+        Sc=3
         start = time.time()
-        Fingerprint.fingerprintP(Template_Path_Little,DataBase_path_Little,seuil_long,seuil_larg,seuil_rot,n_cpu)
+        Fingerprint.fingerprint_VGP(Template_Path_Little,DataBase_path_Little,seuil_long,seuil_larg,seuil_rot,Sc,n_cpu,precision)
         #print('resultats=', results)
         end = time.time()
-        print("Parallel (little DB):exécution time(en secondes): %f" %float( end - start)) 
+        with open('../results/FingerprintHybridLittleDB.txt','w') as f:
+            print("Hybrid (little DB):exécution time(s): %f" %float( end - start),file=f) 
 
 if __name__ == '__main__':
     unittest.main()
